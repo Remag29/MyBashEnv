b29edf992e65d8baf3f69e5db26eb1a444b6da09
Remag29 <73289897+Remag29@users.noreply.github.com>
2023-10-01 15:47:41 +0200

    Correction partie Starship

--- script.sh
+++ script.sh
@@ -2,31 +2,32 @@
 
 echo "Starting the script"
 
-# Install dependencies
-echo "Installing dependencies..."
-sudo apt install curl -y
-sudo apt install unzip -y
-echo "Dependencies installed !"
-
 # Start with updating the system
 echo "Updating the system..."
 sudo apt update
 sudo apt upgrade -y
 echo "System updated !"
 
+# Install dependencies
+echo "Installing dependencies..."
+sudo apt install unzip -y
+echo "Dependencies installed !"
+
 # Install and configure Starship
 echo "Installing Starship..."
 ## Install Fonts
 echo "  |--> Installing Fonts..."
 sudo apt install fontconfig -y
-curl https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip -o JetBrainsMono.zip
-unzip JetBrainsMono.zip -d /usr/share/fonts/
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip -q
+sudo unzip JetBrainsMono.zip -d /usr/share/fonts/
+sudo rm /usr/share/fonts/readme.md
+sudo rm /usr/share/fonts/OFL.txt
 rm JetBrainsMono.zip
 fc-cache -f -v
 
 ## Install Starship
 echo "  |--> Installing Starship..."
-curl -fsSL https://starship.rs/install.sh | bash
+sudo curl -fsSL https://starship.rs/install.sh | sh
 
 ## Edit .bashrc
 echo "  |--> Editing .bashrc..."
@@ -35,4 +36,5 @@ echo 'eval "$(starship init bash)"' >> ~/.bashrc
 ## Download Starship config file
 echo "  |--> Downloading Starship config file..."
 mkdir -p ~/.config
-curl https://raw.githubusercontent.com/Remag29/MyCustomShell/main/bash/starship.toml -o ~/.config/starship.toml
+wget https://raw.githubusercontent.com/Remag29/MyCustomShell/main/bash/starship.toml -q
+mv starship.toml ~/.config/
